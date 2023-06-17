9644895514f3be949d13bf42fe319778f1f71209
Patrick Sparks <pto1026@gmail.com>
2023-06-17 14:24:14 -0600

    quick

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final game = MainApp();
+  runApp(GameWidget(
+    game: game,
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends FlameGame with SingleGameInstance {
+  MainApp({super.camera, super.children});
+
+  late final World world;
+  late final CameraComponent cameraComponent;
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    world = World();
+    await add(world);
+    cameraComponent = CameraComponent(world: world);
+    await add(cameraComponent);
+    world.add(SpriteThing());
+  }
+}
+
+class SpriteThing extends PositionComponent with HasGameRef<MainApp> {
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(100, 100);
+    position = Vector2(game.cameraComponent.viewfinder.anchor.x,
+        game.cameraComponent.viewfinder.anchor.y);
+    anchor = Anchor.center;
   }
 }
